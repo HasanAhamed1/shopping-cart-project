8ff721787ff2acab12018fe2234c35049a6c9b9b
HasanAhamed1 <hasanahmed95695@gmail.com>
2022-03-27 23:50:39 +0600

    submit

--- src/component/Header/Header.js
+++ src/component/Header/Header.js
@@ -4,8 +4,7 @@ import '../Header/Header.css';
 const Header = () => {
     return (
         <div className='header'>
-            <h1>Bag Store</h1>
-            <h3>Choose any 4</h3>
+            <h1><span className='headstyle'>Bag. </span>Bazar</h1>
         </div>
     );
 };
