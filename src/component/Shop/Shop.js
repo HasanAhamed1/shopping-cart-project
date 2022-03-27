0fd2a071bcbca0f9783beac36df803dcddbd8641
HasanAhamed1 <hasanahmed95695@gmail.com>
2022-03-27 23:13:58 +0600

    comment_added

--- src/component/Shop/Shop.js
+++ src/component/Shop/Shop.js
@@ -10,7 +10,7 @@ const Shop = () => {
   const [cart, setCart] = useState([]);
 
   const [clearChossenProduct, setClearChoosenProduct] = useState(0);
-// FETCH JSON FILE
+  // FETCH JSON FILE
   useEffect(() => {
     fetch("data.json")
       .then((res) => res.json())
@@ -28,7 +28,7 @@ const Shop = () => {
   const chooseOne = () => {
     //USED Math.random FOR SHOWING RANDOM ONE
     const newProduct = newCart[Math.floor(Math.random() * newCart.length)];
-    setProduct(newProduct);//SET THE RANDOM ONE INTO THE USESTATE()
+    setProduct(newProduct); //SET THE RANDOM ONE INTO THE USESTATE()
   };
 
   const chooseAgain = () => {
@@ -38,21 +38,22 @@ const Shop = () => {
 
     // FOR REMOVING HTML FILE IN THE CART
     var paras = document.getElementsByClassName("cart-items");
-    while(paras[0]) {
-        paras[0].parentNode.removeChild(paras[0]);
+    while (paras[0]) {
+      paras[0].parentNode.removeChild(paras[0]);
     }
     //REMOVE HTML FILE  SHOWED IN RANDOM
     var paras = document.getElementsByClassName("random");
-    while(paras[0]) {
-        paras[0].parentNode.removeChild(paras[0]);
+    while (paras[0]) {
+      paras[0].parentNode.removeChild(paras[0]);
     }
-    
+
     const newChooseProduct = clearChossenProduct;
     setClearChoosenProduct(newChooseProduct);
   };
 
   return (
     <div className="container">
+      {/* retrun product to the app.js file */}
       <div className="products_container">
         {products.map((product) => (
           <Product
@@ -62,7 +63,8 @@ const Shop = () => {
           ></Product>
         ))}
       </div>
-
+      
+      {/* retrun product to the cart.js file */}
       <div className="cart_container">
         {
           <Cart
